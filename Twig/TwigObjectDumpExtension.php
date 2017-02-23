0d4044a10ff84ad68535ee0c2b8bca82b85164d3
Denis Roussel <denis.roussel@gmail.com>
2017-02-23 11:26:19 +0100

    Fix CS

--- Twig/TwigObjectDumpExtension.php
+++ Twig/TwigObjectDumpExtension.php
@@ -40,7 +40,7 @@ class TwigObjectDumpExtension extends \Twig_Extension
             $output .= sprintf(
                 '    <span class="twig-object-dump-key">%s()</span> %s<br>',
                 $method->getName(),
-                $method->getReturnType() ? ': ' . $method->getReturnType() : ''
+                $method->getReturnType() ? ': '.$method->getReturnType() : ''
             );
         }
         $output = sprintf('<pre class="twig-object-dump">%s</pre>', $output);
@@ -49,6 +49,7 @@ class TwigObjectDumpExtension extends \Twig_Extension
         $style = <<<HTML
 <style> pre.twig-object-dump { display: block; white-space: pre; padding: 5px; } pre.twig-object-dump span { display: inline; } pre.twig-object-dump{display: block; padding: 5px; background-color:#18171B; color:#FF8400; line-height:1.2em; font:12px Menlo, Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:99999; word-break: normal}pre.twig-object-dump .twig-object-dump-key{color:#56DB3A}</style>
 HTML;
-        return $style . $output;
+
+        return $style.$output;
     }
 }
